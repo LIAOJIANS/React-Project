8faa9b87f0b29e153415256a3fbaab3a21cc8356
LIAOJIANS <272781702@qq.com>
2019-08-06 20:55:02 +0800

    环境搭建完成

---
+++ src/containers/main/main.jsx
@@ -0,0 +1,14 @@
+/*
+*  注册路由组件
+* */
+import React, { Component } from 'react'
+
+export default class Register extends Component {
+   render() {
+       return (
+           <div>
+                Register
+           </div>
+       )
+   }
+}
